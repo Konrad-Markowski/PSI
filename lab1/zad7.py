49ffe5bdbdbfdf218650278d3eb91b91a93d1682
Konrad Markowski <115549383+Konrad-Markowski@users.noreply.github.com>
2024-10-15 15:22:33 +0200

    Update zad7.py

--- lab1/zad7.py
+++ lab1/zad7.py
@@ -2,10 +2,25 @@ import asyncio
 
 
 async def krojenie() -> None:
+    print("Kroję...")
     await asyncio.sleep(2)
     print("Pokrojone")
 
 
 async def gotowanie() -> None:
+    print("Gotuję...")
     await asyncio.sleep(4)
-    print("Pokrojone")
+    print("Ugotowane")
+
+
+async def smazenie() -> None:
+    print("Smażę...")
+    await asyncio.sleep(3)
+    print("Usmażone")
+
+async def main() -> None:
+    await asyncio.gather(krojenie(), gotowanie(), smazenie())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
