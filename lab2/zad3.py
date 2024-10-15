ad7a2f5c252c87773fc1dabadeff5a99b258f243
Konrad-Markowski <k.markowski2002@gmail.com>
2024-10-15 18:14:38 +0200

    Update zad3.py

--- lab2/zad3.py
+++ lab2/zad3.py
@@ -6,9 +6,26 @@ import aiohttp
    różnych (dowolnych) stron internetowych w sposób współbieżny."""
 
 
-async def download_page(url: str) -> str:
+async def download_page(urls: str) -> str:
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(urls) as response:
             return await response.text()
 
 
+async def download_all(urls: list) -> list:
+    tasks = [download_page(url) for url in urls]
+    return await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    urls = [
+        "http://www.google.com",
+        "https://www.example.org",
+        "https://stackoverflow.com/",
+        "https://www.wikipedia.org"
+    ]
+    results = asyncio.run(download_all(urls))
+    for i, zawartosc in enumerate(results):
+        print(f"Zawartość strony: {urls[i]}")
+        print(zawartosc[:2000])
+        print("-------------------------------------------------------------------------------------")
